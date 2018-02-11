281df8dc25acca7aa758fc51ad119b4ab64bfd37
NightCatSama <642163903@qq.com>
2018-02-11 12:40:35 +0800

    [Style]: Add annotation

--- backend/src/server/server.go
+++ backend/src/server/server.go
@@ -11,14 +11,18 @@ import (
 )
 
 func main() {
+	// 链接数据库 "mongo/mongo.go"
 	go mongo.LinkDb()
 
+	// 处理请求
 	http.HandleFunc("/addUser", controller.HandleAddUser)
 	http.HandleFunc("/users", controller.HandleGetUsers)
 
+	// 开启服务器
 	sPort := ":" + config.Conf.Server.Port
 	fmt.Printf("[Server is opened]: localhost%s \n", sPort)
 	err := http.ListenAndServe(sPort, nil)
+	
 	if err != nil {
 		log.Fatal("Server is open failed: ", err)
 	}
